4f0a8d628d3a74fd2cba2a40e6a272d826f14a53
naybala <naybalaismylife.nbl@gmail.com>
2026-09-01 21:18:25 +0630

    feat: add logic to BannerResource to handle dynamic media URL generation and distinguish between image and video fields

--- app/Http/Resources/Api/BannerResource.php
+++ app/Http/Resources/Api/BannerResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\Api;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Str;
 
 class BannerResource extends JsonResource
 {
@@ -14,11 +15,26 @@ class BannerResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $type = $this->type ?? 'image';
+        $mediaPath = $this->image;
+
+        $mediaUrl = null;
+        if ($mediaPath) {
+            if (Str::startsWith($mediaPath, ['http://', 'https://'])) {
+                $mediaUrl = $mediaPath;
+            } elseif (config('app.aws_url')) {
+                $mediaUrl = rtrim(config('app.aws_url'), '/') . '/' . ltrim($mediaPath, '/');
+            } else {
+                $mediaUrl = asset('storage/' . ltrim($mediaPath, '/'));
+            }
+        }
+
         return [
             'id' => $this->id,
             'title' => $this->title,
-            'type' => $this->type ?? 'image',
-            'image' => asset(config('app.aws_url') . '/' . $this->image),
+            'type' => $type,
+            'image' => $type === 'image' ? $mediaUrl : $mediaUrl,
+            'video' => $type === 'video' ? $mediaUrl : null,
             'link' => $this->link,
         ];
     }
